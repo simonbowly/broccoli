36b31b2e46fc807b9dffc00c3c492d2e4128e0a3
Simon Bowly <simon.bowly@gmail.com>
2019-10-09 15:00:41 +1100

    Set install script to use 'base' branch for installation.

--- install-broccoli.sh
+++ install-broccoli.sh
@@ -2,6 +2,6 @@ apt-get -q -y install python3 python3-pip git net-tools
 /usr/bin/pip3 install pip --upgrade
 mkdir -p /usr/local/src
 rm -rf /usr/local/src/broccoli
-git clone git@192.168.1.101:simonbowly/broccoli.git /usr/local/src/broccoli
+git clone git@192.168.1.101:simonbowly/broccoli.git /usr/local/src/broccoli --branch base
 /usr/local/bin/pip install /usr/local/src/broccoli --upgrade
-broccoli-update master
+broccoli-update origin/base
